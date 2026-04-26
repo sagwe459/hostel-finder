be2a666379ee7ea5e4e8bdecbafab5351df90971
sagwe459 <sagwejob4@gmail.com>
2026-04-26 14:48:27 +0300

    Add PWA support with manifest, icons, and service worker

---
+++ hostel-finder/frontend/public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'homefinder-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => response)
+      .catch(() => caches.match(event.request))
+  );
+});
